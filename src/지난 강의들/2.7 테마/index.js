dd7e28340e773a77182089040ef9e3f97046cbef
장성민 <aristatait@gmail.com>
2022-12-26 11:00:05 +0900

    feat: 2.7 테마

--- src/지난 강의들/2.7 테마/index.js
+++ src/지난 강의들/2.7 테마/index.js
@@ -3,6 +3,10 @@ import ReactDOM from "react-dom/client";
 import { ThemeProvider } from "styled-components";
 import App from "./App";
 
+// =============================================================================
+// 2.7 Theme
+// ThemeProvider 로 App 을 감싸면 App 에서 theme props 에 접근 할 수 있다
+// =============================================================================
 const darkTheme = {
   textColor: "whitesmoke",
   backgroundColor: "#111"
