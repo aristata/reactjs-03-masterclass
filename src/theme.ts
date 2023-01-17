a5ea0f762ade1ff6268af69ec2a947b63c1e9795
장성민 <aristatait@gmail.com>
2023-01-17 10:20:17 +0900

    feat: 3.6 themes

---
+++ src/theme.ts
@@ -0,0 +1,13 @@
+import { DefaultTheme } from "styled-components";
+
+export const lightTheme: DefaultTheme = {
+  backgroundColor: "white",
+  textColor: "black",
+  buttonColor: "tomato"
+};
+
+export const darkTheme: DefaultTheme = {
+  backgroundColor: "black",
+  textColor: "white",
+  buttonColor: "teal"
+};
---
+++ src/지난 강의들/ch03_typescript/3.5 Forms/App.tsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+
+function App() {
+  const [value, setValue] = useState("");
+  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+    // 타입스크립트에서는 가능한 any 대신에 구체적인 타입을 명시해야 한다
+    const {
+      currentTarget: { value }
+    } = event;
+    setValue(value);
+  };
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log(`Hello ${value}`);
+  };
+  return (
+    <form onSubmit={onSubmit}>
+      <input
+        value={value}
+        onChange={onChange}
+        placeholder="Input your name"
+      ></input>
+      <button type="submit">Submit</button>
+    </form>
+  );
+}
+
+export default App;
