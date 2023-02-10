690fc823aae58151ce9cff71ccc1d87da892947b
장성민 <aristatait@gmail.com>
2023-02-10 16:22:21 +0900

    feat: 폼 밸리데이션

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,36 +1,32 @@
 import { useState } from "react";
 import { useForm } from "react-hook-form";
 
-/**
- * 일반적인 방법으로 투두 리스트를 만든다면, 아래와 같이 만들 수 있다
- * 데이터가 저장될 스테이트를 선언하고, 데이터를 입력받을 인풋, 데이터를 변경할 이벤트 등을 구현해야 한다
- */
 const ToDoList = () => {
-  /* const [toDo, setToDo] = useState("");
-  const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const {
-      currentTarget: { value }
-    } = event;
-    setToDo(value);
-  };
-  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    console.log(toDo);
-  }; */
+  // handleSubmit(성공, 실패)
+  // 첫번째 인자로 성공시 실행할 함수를 받는다
+  // 두번째 인자는 실패시 실행할 함수인데, 선택사항이다
+  // 폼이 제출(submit) 되었을 때, 모든 밸리데이션이 통과되었다면, 성공 함수를 실행한다
+  // 밸리데이션이 하나라도 통과되지 못했다면, 실패 함수가 실행된다
 
-  /**
-   * react-hook-form 을 사용하면, 위의 같은 절차들을 간소화 할 수 있다
-   * 사용하는 방법은 useForm 훅을 사용하는 것이다
-   * 해당 훅을 사용하면 다양한 기능들을 하는 함수 또는 상태들을 리턴받을 수 있다
-   * register 는 폼을 등록하는 함수 이다
-   * watch 는 폼의 데이터를 조회하는 함수 이다
-   */
-  const { register, watch } = useForm();
-  console.log(watch());
+  // formState
+  // 현재 폼의 상태를 확인할 수 있다
+  const { register, handleSubmit, formState } = useForm();
+  const onValid = (formData: any) => {
+    console.log(formData);
+  };
+  console.log(formState.errors);
+
+  // validation 은 register 의 두번째 인자로 넣을 수 있다
   return (
     <div>
-      <form>
-        <input {...register("email")} placeholder={"email 을 입력하세요"} />
+      <form
+        style={{ display: "flex", flexDirection: "column", rowGap: "8px" }}
+        onSubmit={handleSubmit(onValid)}
+      >
+        <input
+          {...register("email", { required: true, minLength: 5 })}
+          placeholder={"email 을 입력하세요"}
+        />
         <input
           {...register("firstName")}
           placeholder={"firstName 을 입력하세요"}
@@ -44,11 +40,17 @@ const ToDoList = () => {
           placeholder={"userName 을 입력하세요"}
         />
         <input
-          {...register("password")}
+          {...register("password", {
+            required: "비밀번호는 필수값 입니다",
+            minLength: {
+              value: 8,
+              message: "비밀번호는 최소 8자 이상이어야 합니다"
+            }
+          })}
           placeholder={"password 을 입력하세요"}
         />
         <input
-          {...register("passwordConfirm")}
+          {...register("passwordConfirm", { required: true, minLength: 5 })}
           placeholder={"passwordConfirm 을 입력하세요"}
         />
         <button>추가하기</button>
