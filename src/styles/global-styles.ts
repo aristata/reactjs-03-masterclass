d533729fb7b8de6e64cd47a13007011a1ba14e8a
장성민 <aristatait@gmail.com>
2023-02-14 13:41:54 +0900

    feat: form css 적용

--- src/styles/global-styles.ts
+++ src/styles/global-styles.ts
@@ -6,4 +6,22 @@ export const GlobalStyle = createGlobalStyle`
   ${reset}
 
   // custom global styles
+  @font-face {
+    font-family: 'Pretendard-Regular';
+    src: url('https://cdn.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Regular.woff') format('woff');
+    font-weight: 400;
+    font-style: normal;
+  }
+
+  @font-face {
+    font-family: 'Cafe24Ssurround';
+    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2105_2@1.0/Cafe24Ssurround.woff') format('woff');
+    font-weight: normal;
+    font-style: normal;
+  }
+
+  * {
+    font-family: 'Cafe24Ssurround';
+  }
+
 `;
