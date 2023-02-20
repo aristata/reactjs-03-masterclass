8233db7453f662271bfa082a4f74cf43d2d2b92b
장성민 <aristatait@gmail.com>
2023-02-20 09:32:45 +0900

    feat: todo 카테고리 추가

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -1,12 +1,32 @@
-import { ToDoData } from "../atoms/todos";
+import { useSetRecoilState } from "recoil";
+import { ToDoData, toDoState } from "../atoms/todos";
 
-const ToDo = ({ text }: ToDoData) => {
+const ToDo = ({ text, category, id }: ToDoData) => {
+  const setToDo = useSetRecoilState(toDoState);
+  const changeCategory = (event: React.MouseEvent<HTMLButtonElement>) => {
+    const {
+      currentTarget: { name }
+    } = event;
+    console.log(`${name} 버튼 클릭됨`);
+  };
   return (
     <li>
       <span>{text}</span>
-      <button>Doing</button>
-      <button>To Do</button>
-      <button>Done</button>
+      {category !== "DOING" && (
+        <button name="DOING" onClick={changeCategory}>
+          Doing
+        </button>
+      )}
+      {category !== "TO_DO" && (
+        <button name="TO_DO" onClick={changeCategory}>
+          To Do
+        </button>
+      )}
+      {category !== "DONE" && (
+        <button name="DONE" onClick={changeCategory}>
+          Done
+        </button>
+      )}
     </li>
   );
 };
