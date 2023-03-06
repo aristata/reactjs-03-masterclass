4db310c1aa3895d4e8ae44548754239f0fc6c0d5
장성민 <aristatait@gmail.com>
2023-03-06 11:37:32 +0900

    feat: recoil selector

--- src/atoms/todos.ts
+++ src/atoms/todos.ts
@@ -1,4 +1,4 @@
-import { atom } from "recoil";
+import { atom, selector } from "recoil";
 
 export interface ToDoData {
   id: number;
@@ -9,4 +9,18 @@ export interface ToDoData {
 export const toDoState = atom<ToDoData[]>({
   key: "toDo",
   default: []
+});
+
+export const toDoCategoryState = atom({
+  key: "toDoCategory",
+  default: "TO_DO"
+});
+
+export const toDoSelector = selector({
+  key: "toDoSelector",
+  get: ({ get }) => {
+    const toDos = get(toDoState);
+    const category = get(toDoCategoryState);
+    return toDos.filter((toDo) => toDo.category === category);
+  }
 });
--- src/pages/ToDoList.tsx
+++ src/pages/ToDoList.tsx
@@ -1,21 +1,35 @@
-import { useRecoilValue } from "recoil";
-import { toDoState } from "../atoms/todos";
+import { useRecoilState, useRecoilValue } from "recoil";
+import { toDoCategoryState, toDoSelector } from "../atoms/todos";
 import CreateToDo from "../components/CreateToDo";
 import ToDo from "../components/ToDo";
 
 const ToDoList = () => {
-  const toDos = useRecoilValue(toDoState);
-  console.log("ToDos", toDos);
+  const toDos = useRecoilValue(toDoSelector);
+  const [category, setCategory] = useRecoilState(toDoCategoryState);
+  const onChangeHandler = (event: React.FormEvent<HTMLSelectElement>) => {
+    setCategory(event.currentTarget.value);
+  };
   return (
     <div>
-      <h1>To Dos</h1>
+      <h1>할 일 목록</h1>
       <hr />
+      <select value={category} onChange={onChangeHandler}>
+        <option value={"TO_DO"}>해야할 일</option>
+        <option value={"DOING"}>하는중인 일</option>
+        <option value={"DONE"}>다한 일</option>
+      </select>
       <CreateToDo />
-      <ul>
-        {toDos.map((toDo) => (
-          <ToDo key={toDo.id} {...toDo} />
-        ))}
-      </ul>
+      {toDos.length > 0 ? (
+        <ul>
+          {toDos.map((toDo) => (
+            <ToDo key={toDo.id} {...toDo} />
+          ))}
+        </ul>
+      ) : (
+        <>
+          <p>없습니다</p>
+        </>
+      )}
     </div>
   );
 };
