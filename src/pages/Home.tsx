a4b9b5c2d63737744de3e9c237fd2237e4db9eaf
장성민 <aristatait@gmail.com>
2023-07-25 16:10:16 +0900

    fix: bgPhoto 어트리뷰트 이름 변경

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -19,14 +19,14 @@ const Loader = styled.div`
   align-items: center;
 `;
 
-const Banner = styled.div<{ bgPhoto: string }>`
+const Banner = styled.div<{ bg_photo: string }>`
   height: 100vh;
   display: flex;
   flex-direction: column;
   justify-content: center;
   padding: 60px;
   background-image: linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 1)),
-    url(${(props) => props.bgPhoto});
+    url(${(props) => props.bg_photo});
   background-size: cover;
 `;
 
@@ -53,9 +53,9 @@ const Row = styled(motion.div)`
   position: absolute;
 `;
 
-const Box = styled(motion.div)<{ bgPhoto: string }>`
+const Box = styled(motion.div)<{ bg_photo: string }>`
   background-color: whitesmoke;
-  background-image: url(${(props) => props.bgPhoto});
+  background-image: url(${(props) => props.bg_photo});
   background-size: cover;
   background-position: center center;
   height: 200px;
@@ -206,7 +206,7 @@ const Home = () => {
         <>
           <Banner
             onClick={incrementIndex}
-            bgPhoto={makeImagePath(data?.results[0].backdrop_path || "")}
+            bg_photo={makeImagePath(data?.results[0].backdrop_path || "")}
           >
             <Title>{data?.results[0].title}</Title>
             <Overview>{data?.results[0].overview}</Overview>
@@ -231,7 +231,7 @@ const Home = () => {
                       initial={"normal"}
                       whileHover={"hover"}
                       transition={{ type: "tween" }}
-                      bgPhoto={makeImagePath(movie.backdrop_path, "w500")}
+                      bg_photo={makeImagePath(movie.backdrop_path, "w500")}
                       layoutId={movie.id + ""}
                       onClick={() => onBoxClicked(movie.id)}
                     >
